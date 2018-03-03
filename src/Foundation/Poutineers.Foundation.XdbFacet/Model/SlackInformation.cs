e98fbacafb4cfd00421e06f23d8604c8bfa2e222
Ryan <ryan.bailey@akqa.com>
2018-03-03 16:24:30 +1300

    Poutineers.Foundation.XdbFacet project with custom facet defined.

---
+++ src/Foundation/Poutineers.Foundation.XdbFacet/Model/SlackInformation.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.XConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Poutineers.Foundation.XdbFacet.Model
+{
+    [FacetKey(FacetName)]
+    public class SlackInformation : Facet
+    {
+        public const string FacetName = "SlackInformation";
+
+        public string SlackId { get; set; }
+        public string SlackMessages { get; set; }
+    }
+}
---
+++ src/Foundation/Poutineers.Foundation.XdbFacet/Model/xConnectSlackModel.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.XConnect.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.XConnect.Collection.Model;
+using Sitecore.XConnect;
+
+namespace Poutineers.Foundation.XdbFacet.Model
+{
+    public class xConnectSlackModel
+    {
+        public static XdbModel Model { get; } = BuildModel();
+
+        private static XdbModel BuildModel()
+        {
+            var builder = new XdbModelBuilder("xConnectSlackModel", new XdbModelVersion(1, 0));
+            builder.ReferenceModel(CollectionModel.Model);
+            builder.DefineFacet<Contact, SlackInformation>(SlackInformation.FacetName);
+
+            return builder.BuildModel();
+        }
+    }
+}
